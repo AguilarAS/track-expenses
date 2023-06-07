a26954350bc66134a92c5c64a4e14203de9763b1
Andres Aguilar <andres.aguilar@unosquare.com>
2023-06-06 23:20:22 -0600

    partial validation

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,39 @@
-import express, { Request, Response } from "express";
+import express, { json, urlencoded, Request, Response } from "express";
+import cors from "cors"
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import userRouter from "./routers/users";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) =>
-    res.send("Hello World")
-);
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "track expenses",
+        version: "1.0.0",
+    },
+    servers: [
+        {
+            url: "http://localhost:3000",
+            description: "Local development server",
+        },
+    ],
+};
+
+const openapiSpecification = swaggerJSDoc({
+    swaggerDefinition,
+    apis: ["./routers/*.ts"],
+});
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(openapiSpecification));
+app.use("/swagger.json", (req: Request, res: Response) => res.json(openapiSpecification).status(200));
+
+
+app.use("/users", userRouter);
 
 app.listen(port, () => console.log(`Application started on port: ${port}`));
